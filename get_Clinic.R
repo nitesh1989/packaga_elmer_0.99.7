de1577b1e60973257b1db0bdc094b2634c9a4bb0
nitesh1989 <nitesh.turaga@gmail.com>
2015-08-24 15:55:47 -0400

    Commit ELMER functions. Only for TCGA.

--- get_Clinic.R
+++ get_Clinic.R
@@ -11,12 +11,7 @@ library(ELMER, quietly=TRUE, warn.conflicts=FALSE,verbose = FALSE)
 # 3. argument type: 0: No argument, 1: required, 2: optional
 # 4. target data type
 option_specification = matrix(c(
-  'disease','d',1,'character',
-  'Meth','m',1,'character',
-  'RNA', 'g', 1, 'character',
-  'Clinic', 'c', 1, 'character',
-  'RNAtype','r',2,'character',
-  'Methfilter','mf',2,'integer'
+  'disease','i',1,'character'
 ), byrow=TRUE, ncol=4);
 
 # Parse options
@@ -27,7 +22,4 @@ if(is.null(options$disease)){
    stop("The disease name should be from TCGA")
 }
 
-# Download all TCGA data
-getTCGA(options$disease, Meth = options$Meth, RNA = options$RNA,
-	 Clinic = options$Clinic,basedir = "./Data", RNAtype = options$RNAtype,
-	 Methfilter = options$Methfilter)
+getClinic(options$disease, basedir = "./Data")
